b931ca656fd603739bbafbd721aa761835cfa3a0
taru <taru253@gmail.com>
2024-12-05 17:22:28 +0530

    "Updated package dependencies, added react-icons, and refactored layout and page components with new CSS and JSX code."

---
+++ src/components/Section1.jsx
@@ -0,0 +1,9 @@
+export default function Section1() {
+  return (
+    <section className="py-16">
+        <div className="container mx-auto md:px-20">
+            <h1>Trending</h1>
+        </div>  
+    </section>
+  )
+}
