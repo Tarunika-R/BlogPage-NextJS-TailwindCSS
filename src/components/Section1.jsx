f8fcfd7cad69d974caa6c0fc35f2546cb83af1a7
taru <taru253@gmail.com>
2024-12-05 20:07:31 +0530

    "Updated package-lock.json and package.json with new dependencies, added Swiper and slick-carousel libraries, modified Section1.jsx to use Swiper for slides, and updated globals.css."

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,41 +1,107 @@
-import Image from "next/image"
-import Link from "next/link"
-import Author from "@/components/_child/Author"
+'use client'; 
+
+import Image from "next/image";
+import Link from "next/link";
+import Author from "@/components/_child/Author";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Section1() {
-    {/*const bg ={
-        background:"url('/images/banner.png') no-repeat",
-        backgroundPosition:"right"
-    }*/}
   return (
-    <section className="py-16">
-        <div className="container mx-auto md:px-20">
-            <h1 className="font-semibold text-4xl pb-12 text-center">Trending</h1>
-            {Slide()}
-        </div>  
+    <section className="py-15">
+      <div className="container mx-auto md:px-20">
+        <h1 className="font-semibold text-4xl pb-12 text-center">Trending</h1>
+        <Slide />
+      </div>
     </section>
-  )
+  );
 }
 
-function Slide(){
-    return (
-        <div className="grid md:grid-cols-2 ">
-            <div className="image">
-                <Link href="/" legacyBehavior><a><Image src={"/images/image1.jpg"} width={600} height={600}/></a></Link>
+function Slide() {
+  const slideData = [
+    {
+      id: 1,
+      image: "/images/image1.jpg",
+      category: "Business, Travel",
+      date: "July 3, 2022",
+      title:
+        "Your most unhappy customers are your greatest source of learning",
+      description:
+        "Even the all-powerful Pointing has no control about the blind texts. It is an almost unorthographic life. One day, however, a small line of blind text decided to leave for the World of Grammar.",
+    },
+    {
+      id: 2,
+      image: "/images/image2.jpg",
+      category: "Technology, Innovation",
+      date: "July 10, 2022",
+      title: "Technology is the heart of the modern world",
+      description:
+        "Blind texts are meaningless, but meaningful technology can change the world. Dive into the future of innovation and its impact on our lives.",
+    },
+    {
+      id: 3,
+      image: "/images/image3.jpg",
+      category: "Lifestyle, Health",
+      date: "July 15, 2022",
+      title: "A healthy lifestyle begins with small steps",
+      description:
+        "Explore the power of small, daily habits in transforming your lifestyle for a healthier, happier you.",
+    },
+  ];
+
+  return (
+    <Swiper
+      modules={[Autoplay, Navigation, Pagination]}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      spaceBetween={30}
+      slidesPerView={1}
+    >
+      {slideData.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <div className="grid md:grid-cols-2 gap-4">
+            <div className="image relative w-full h-[400px">
+              <Link href="/" legacyBehavior>
+                <a>
+                  <Image
+                    src={slide.image}
+                    alt="Slide Image"
+                    width={600}
+                    height={600}
+                  />
+                </a>
+              </Link>
             </div>
             <div className="info flex justify-center flex-col">
-                <div className="cat">
-                    <Link href="/" legacyBehavior><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
-                    <Link href="/" legacyBehavior><a className="text-gray-800 hover:text-gray-600">-July 3, 2022</a></Link>
-                </div>
-                <div className="title">
-                    <Link href="/" legacyBehavior><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
-                </div>
-                <p className="text-gray-500 py-3">
-                    Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. One day however a small line of blind test by the name of Lorem Ipsum decided to leave for the World of Grammar.
-                </p>
-                <Author/>
+              <div className="cat">
+                <Link href="/" legacyBehavior>
+                  <a className="text-orange-600 hover:text-orange-800">
+                    {slide.category}
+                  </a>
+                </Link>
+                <Link href="/" legacyBehavior>
+                  <a className="text-gray-800 hover:text-gray-600">
+                    - {slide.date}
+                  </a>
+                </Link>
+              </div>
+              <div className="title">
+                <Link href="/" legacyBehavior>
+                  <a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">
+                    {slide.title}
+                  </a>
+                </Link>
+              </div>
+              <p className="text-gray-500 py-3">{slide.description}</p>
+              <Author />
             </div>
-        </div>
-    )
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
